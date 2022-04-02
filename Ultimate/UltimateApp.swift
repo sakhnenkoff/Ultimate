c195285be2ad209bf4f2b721ef3bd1ae4e48af59
sakhnenkoff <39955656+sakhnenkoff@users.noreply.github.com>
2022-04-02 15:30:56 +0300

    Initial Commit

---
+++ Ultimate/UltimateApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimateApp.swift
+//  Ultimate
+//
+//  Created by Matthew Sakhnenko on 02.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct UltimateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
